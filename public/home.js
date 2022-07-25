b1ecdcb64edbbd69d25302ed1cb922c694d6840e
Margo Kelley <margo.kelley13@gmail.com>
2022-07-25 16:46:43 -0400

    add login/create to home page

--- public/home.js
+++ public/home.js
@@ -1,8 +1,14 @@
 function Home(){
+
   return (
     <div className="container-fluid">
       <p className="page-head">Welcome to BADBANK</p>
-      <p className="page-subtitle">Bad to the Bone</p>
+      <br/>
+      <br/>
+      <div className="options">
+        <Link to="/login/"><button className="btn btn-light">Login</button></Link><p id="p">or</p><Link to="/createaccount/"><button className="btn btn-light">Create Account</button></Link>
+      </div>
+      <br/>
       <Card
         body={
           <img
