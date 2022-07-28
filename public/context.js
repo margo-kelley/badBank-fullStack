fe3454af74561355f384b61452f36da4801cf82f
Margo Kelley <margo.kelley13@gmail.com>
2022-07-28 13:29:44 -0400

    add firebase config

--- public/context.js
+++ public/context.js
@@ -1,7 +1,21 @@
 const Route       = ReactRouterDOM.Route;
 const Link        = ReactRouterDOM.Link;
 const HashRouter  = ReactRouterDOM.HashRouter;
-const UserContext = React.createContext(null);
+// const UserCtx = React.createContext(null);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBU-nWEb2C6TB9hwcJgl0oK5vDsm4b3Bjc",
+  authDomain: "badbank-addf2.firebaseapp.com",
+  projectId: "badbank-addf2",
+  storageBucket: "badbank-addf2.appspot.com",
+  messagingSenderId: "595019698861",
+  appId: "1:595019698861:web:84ec7978189ca7d3d02c24",
+};
+
+// initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
 
 function Card(props){
   function classes(){
