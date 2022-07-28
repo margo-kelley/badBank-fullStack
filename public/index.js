95d01532cda60f2e84887d6ca096b8cfc3754605
Margo Kelley <margo.kelley13@gmail.com>
2022-07-28 13:31:50 -0400

    add logout page

--- public/index.js
+++ public/index.js
@@ -1,35 +1,25 @@
 function Spa() {
   const Route = ReactRouterDOM.Route;
   const HashRouter = ReactRouterDOM.HashRouter;
+  const [user, setUser] = React.useState({});
 
   return (
     <HashRouter>
-      <NavBar />
-      <UserContext.Provider
-        value={{
-          users: [
-            {
-              id: 1,
-              name: "peter",
-              email: "peter@mit.edu",
-              password: "secret",
-              balance: 100,
-            },
-          ],
-        }}
-      >
+      {/* <UserCtx.Provider value={UserCtx}> */}
+        <NavBar setUser={setUser} user={user} />
         <Route path="/" exact component={Home} />
         <Route path="/createaccount/" component={CreateAccount} />
         <Route path="/login/" component={Login} />
+        <Route path="/logout/" component={Logout} />
+          {/* <Login setUser={setUser}/> */}
         <Route path="/deposit/" component={Deposit} />
+          {/* <Deposit user={user}/> */}
         <Route path="/withdraw/" component={Withdraw} />
+          {/* <Withdraw user={user} /> */}
         <Route path="/dash/" component={Dash} />
-      </UserContext.Provider>
+      {/* </UserCtx.Provider> */}
     </HashRouter>
   );
 }
 
-ReactDOM.render(
-  <Spa/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Spa />, document.getElementById("root"));
