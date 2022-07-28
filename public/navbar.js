6a7c61612a375170a0f73cef61ee9d3f4a763ab4
Margo Kelley <margo.kelley13@gmail.com>
2022-07-28 12:43:12 -0400

    add logout to nav

--- public/navbar.js
+++ public/navbar.js
@@ -1,40 +1,70 @@
 function NavBar(){
+  // const UserCtx = React.useContext(UserCtx);
+
   return (
     <>
+      {/* {UserCtx ? ( */}
       <nav className="navbar">
         <div className="container-fluid">
           <a className="navbar-brand mb-1" href="#">
             BADBANK
           </a>
-            <ul className="nav justify-content-start">
-              <li className="nav-item">
-                <a className="nav-link" href="#/CreateAccount/">
-                  Create Account
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#/login/">
-                  Login
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#/deposit/">
-                  Deposit
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#/withdraw/">
-                  Withdraw
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#/dash/">
-                  User Dashboard
-                </a>
-              </li>
-            </ul>
-          </div>
+          <ul className="nav justify-content-start">
+            <li className="nav-item">
+              <a className="nav-link" href="#/CreateAccount/">
+                Create Account
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/login/">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/deposit/">
+                Deposit
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/withdraw/">
+                Withdraw
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav>
+      {/* ) : ( */}
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            BADBANK
+          </a>
+          <ul className="nav justify-content-start">
+            <li className="nav-item">
+              <a className="nav-link" href="#/logout/">
+                Logout
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/deposit/">
+                Deposit
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/withdraw/">
+                Withdraw
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" id="user-email" href="#/dash/">
+                User Email
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      {/* )} */}
     </>
   );
 }
+
