afb44c1b3fd37ceadb5b52c0ee96957516789bb9
Margo Kelley <margo.kelley13@gmail.com>
2022-07-28 12:42:40 -0400

    add logout page

---
+++ public/logout.js
@@ -0,0 +1,59 @@
+function Logout() {
+  const [show, setShow] = React.useState(true);
+  const [status, setStatus] = React.useState("");
+
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="header">Logout</div>
+        <Card
+          bgcolor="grey"
+          txtcolor="black"
+          status={status}
+          body={
+            show ? (
+              <LogoutForm setShow={setShow} setStatus={setStatus} />
+            ) : (
+              <LogoutMsg setShow={setShow} setStatus={setStatus} />
+            )
+          }
+        />
+      </div>
+    </>
+  );
+}
+
+function LogoutMsg(props) {
+  return (
+    <>
+      <h5>Username: Logged Out</h5>
+        <button className="btn btn-light">LOGIN</button>
+    </>
+  );
+}
+
+function LogoutForm(props) {
+
+  function handle() {
+  firebase.auth().signOut();
+  fetch( `/account/logout`)
+  }
+
+  return (
+    <>
+      
+        Display Email here,
+        <br />
+        <br />
+      <div className="logout-confirm">
+        Do you wish to logout?
+        </div>
+        <br />
+        <br/>
+      <button type="submit" className="btn btn-light" onClick={handle}>
+        LOGOUT
+      </button>
+      
+    </>
+  );
+}
