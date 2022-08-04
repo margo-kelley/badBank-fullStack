4c2f82edc18ade9693696417f00716d18da6410a
Margo Kelley <margo.kelley13@gmail.com>
2022-08-03 22:54:46 -0400

    data mapped + styled. 90% finished

--- public/alldata.js
+++ public/alldata.js
@@ -1,14 +1,15 @@
 function AllData(){
-    const [data, setData] = React.useState('');
-    const ctx = React.useContext(UserCtx);
+  const [data, setData] = React.useState([]);
+  const [users, setUsers] = React.useState([]);
 
     React.useEffect(() => {
         // fetch all accounts from API
         fetch('/account/all')
             .then(response => response.json())
             .then(data => {
-                console.log(data);
-                setData(JSON.stringify(data));
+              console.log(data);
+              setData(JSON.stringify(data));
+              setUsers(Object.entries(data));
             });
     }, []);
 
@@ -16,12 +17,27 @@ function AllData(){
       <>
         <div className="container-fluid">
           <p className="header">User Dashboard</p>
-          {/* works until logged in? */}
-          {ctx.user.map((user)=>{
-            return (
-              <div><li>{user.name}</li></div>
-            )
-          })}
+          <div className="card-body">
+
+            <div className="container text-center">
+              <div className="row row-cols-3">
+                <div className="col">
+                {users.map((users, index) => {
+                return (
+                  <ul>
+                    <li><strong>Name:</strong> {users.name}</li>
+                    <li key={index}><strong>Email:</strong> {users.email}</li>
+                    <li><strong>Password:</strong> {users.password}</li>
+                    <li><strong>Balance:</strong> {users.balance}</li>
+                  </ul>
+                );
+              })}
+                </div>
+              </div>
+            </div>
+            <div>
+            </div>
+          </div>
           {data}
         </div>
       </>
